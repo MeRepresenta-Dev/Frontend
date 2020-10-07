3322384095db3f4391d0e71550fe843c368b0e1a
Lucas Soares <lsoares267@gmail.com>
2020-10-07 12:03:53 -0300

    Ajustando o rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import whatsapp from './icons/whatsapp.svg'
-import youtube from './icons/youtube.svg'
-import facebook from './icons/facebook.svg'
-import altec from './icons/altec.png'
-import tiniguimaraes from './icons/tiniguimaraes.png'
-import casa1 from './icons/casa-1.png'
-import zoly from './icons/saibamais-zoly.png'
+import whatsapp from "./icons/whatsapp.svg";
+import youtube from "./icons/youtube.svg";
+import facebook from "./icons/facebook.svg";
+import altec from "./icons/altec.png";
+import tiniguimaraes from "./icons/tiniguimaraes.png";
+import casa1 from "./icons/casa-1.png";
+import zoly from "./icons/saibamais-zoly.png";
+import undef from "./icons/undef.png";
+import ciudadania from "./icons/cidadania-inteligente.png";
+import avina from "./icons/avina.png";
+import omidyar from "./icons/omidyar.png";
+import mattos from "./icons/mattos-filho-veiga-filho-marrey-jr-e-quiroga-advogados-original.png";
+import dataLabel from "./icons/data-lavel.png";
+import silveira from "./icons/silveira_andrade.png";
 
-import './styles.css'
+import "./styles.css";
 
 const Footer = () => {
   return (
@@ -20,19 +27,13 @@ const Footer = () => {
             <nav>
               <ul>
                 <li>
-                  <a href="#">
-                    Quem somos
-                  </a>
+                  <a href="#">Quem somos</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Impacto
-                  </a>
+                  <a href="#">Impacto</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Transparência
-                  </a>
+                  <a href="#">Transparência</a>
                 </li>
               </ul>
             </nav>
@@ -42,34 +43,48 @@ const Footer = () => {
             <nav>
               <ul>
                 <li>
-                  <a href="#">
-                    Candidato
-                  </a>
+                  <a href="#">Candidato</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Eleitor
-                  </a>
+                  <a href="#">Eleitor</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Voluntário
-                  </a>
+                  <a href="#">Voluntário</a>
                 </li>
               </ul>
             </nav>
           </div>
           <div className="financing">
             <span>Financiamento</span>
-            <img src={altec} alt="Uma iniciativa de Altec" />
+            <ul>
+              <li>
+                <img src={ciudadania} alt="Ciudadania Inteligente" />
+              </li>
+              {/* <li>
+                <img src={altec} alt="Uma iniciativa de Altec" />
+              </li> */}
+              <li>
+                <img src={undef} alt="Undef" />
+              </li>
+              <li>
+                <p id="iniciativa-titulo">uma iniciativa de</p>
+              </li>
+              <li>
+                <img src={avina} alt="Avina" />
+                <img src={omidyar} alt="Omidyar" />
+              </li>
+            </ul>
           </div>
           <div className="support">
             <span>Apoio</span>
             <div className="supportWrapper">
               <img src={zoly} alt="Saiba Mais Zoly" />
+              <img src={mattos} alt="Mattos Filho" />
+              <img src={silveira} alt="Silveira Andrade" />
               <img src={casa1} alt="Casa 1" />
               <img src={tiniguimaraes} alt="Tini e Guimarães" />
             </div>
+            <img src={dataLabel} alt="Data Labe" />
           </div>
         </div>
       </div>
@@ -89,19 +104,13 @@ const Footer = () => {
                   <img src={facebook} alt="Facebook" />
                 </li>
                 <li>
-                  <a href="#">
-                    Contato
-                  </a>
+                  <a href="#">Contato</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Perguntas?
-                  </a>
+                  <a href="#">Perguntas?</a>
                 </li>
                 <li>
-                  <a href="#">
-                    Termos de Uso
-                  </a>
+                  <a href="#">Termos de Uso</a>
                 </li>
               </ul>
             </nav>
@@ -109,7 +118,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
