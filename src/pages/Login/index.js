796802f435b869eff88249a7ddb20bd4f08cee13
Lucas Soares <lsoares267@gmail.com>
2020-10-07 11:04:19 -0300

    Ajustando tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,48 +1,38 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom';
-import { Form } from 'react-final-form'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Form } from "react-final-form";
 
-import {
-  Box,
-  Button,
-  Heading,
-} from '@chakra-ui/core'
-import './styles.css'
+import { Box, Button, Heading } from "@chakra-ui/core";
+import "./styles.css";
 
-import validate from '../../utils/validate'
-import InputControl from '../../components/InputControl'
+import validate from "../../utils/validate";
+import InputControl from "../../components/InputControl";
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const onSubmit = async values => {
-  await sleep(300)
-  window.alert(JSON.stringify(values, 0, 2))
-}
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
 export default function Login() {
-
   const history = useHistory();
 
-  const routeChange = () =>{ 
-    let path = `/cadastro`; 
+  const routeChange = () => {
+    let path = `/cadastro`;
     history.push(path);
-  }
+  };
 
   return (
-      <main className="login">
-        <section className="loginWrapper">
-          <Heading className="loginHeading" as="h1" size="2xl">
-            Login
-          </Heading>
-          <Form
-            onSubmit={onSubmit}
-            validate={validate}
-            render={({
-              handleSubmit,
-              form,
-              errors,
-              submitting,
-            }) => (
+    <main className="login">
+      <section className="loginWrapper">
+        <Heading className="loginHeading" as="h1" size="2xl">
+          Login
+        </Heading>
+        <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          render={({ handleSubmit, form, errors, submitting }) => (
             <Box
               as="form"
               p={4}
@@ -59,16 +49,13 @@ export default function Login() {
                 <Button
                   isLoading={submitting}
                   loadingText="Enviando"
-                  variantColor="teal"
+                  variantColor="pink"
+                  variant="solid"
                   type="submit"
                 >
                   Entrar
                 </Button>
-                <Button
-                  variantColor="teal"
-                  variant="outline"
-                  onClick={routeChange}
-                >
+                <Button variantColor="teal" onClick={routeChange}>
                   Cadastre-se
                 </Button>
                 <a className="forgetPassword" href="#">
@@ -76,9 +63,9 @@ export default function Login() {
                 </a>
               </Box>
             </Box>
-            )}
-          />
-        </section>
-      </main>
-  )
+          )}
+        />
+      </section>
+    </main>
+  );
 }
