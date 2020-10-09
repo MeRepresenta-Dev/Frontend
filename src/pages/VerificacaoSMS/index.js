7d2e25046e472c2b3e17e27e23802f3d594beaf5
Lucas Soares <lsoares267@gmail.com>
2020-10-09 20:51:07 -0300

    Tela de verificação de sms

--- src/pages/VerificacaoSMS/index.js
+++ src/pages/VerificacaoSMS/index.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Form } from "react-final-form";
+import { Box, Button, Heading } from "@chakra-ui/core";
+import api from "../../services/api";
+import "./styles.css";
 
-// https://codedaily.io/tutorials/65/Create-a-Segmented-Auto-Moving-SMS-Code-Verification-Input-in-React
+import InputControl from "../../components/InputControl";
 
-export default function Verificacao() {
-  return <h2>Verificacao</h2>;
+const onSubmit = async (values) => {
+  const response = await api.post("/validacao", values);
+
+  if(response.status !== 200){
+    return alert('Código inválido');
+  }
+};
+
+export default function Login() {
+  const history = useHistory();
+
+  const enviarNovamente =  async () => {
+    alert("enviar novamente");
+  };
+
+  return (
+    <main className="login">
+      <section className="loginWrapper">
+        <Heading className="loginHeading" as="h1" size="1xl">
+          Digite o código recebido
+        </Heading>
+        <p id="frase" >Enviamos um SMS de verificação para <strong> {}. </strong> Insira o código de verificação recebido. </p>
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit, form, errors, submitting }) => (
+            <Box
+              as="form"
+              p={4}
+              borderWidth="1px"
+              rounded="lg"
+              shadow="1px 1px 3px rgba(0,0,0,0.3)"
+              onSubmit={handleSubmit}
+            >
+              <br></br>
+              <Box className="loginInputs">
+                <InputControl name="codigo" label="Código de verificação" />
+              </Box>
+              <Box className="loginButtons">
+                <a id="enviarNovamente" className="forgetPassword" onClick={enviarNovamente}>
+                  Enviar novamente
+                </a>
+                <Button id="btnContinuar" isLoading={submitting} loadingText="Enviando"  type="submit" variantColor="teal">
+                  Continuar
+                </Button>
+                
+              </Box>
+            </Box>
+          )}
+        />
+      </section>
+    </main>
+  );
 }
