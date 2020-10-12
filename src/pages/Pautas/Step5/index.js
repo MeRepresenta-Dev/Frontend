7ee937979b1cf00ebc3e187deea1b962ce757f78
Gustavo <Amaralgustavo59@gmail.com>
2020-10-12 20:23:16 -0300

    Pautas 1(Ainda não finalizadas)
    Faltam:
    - Colocar o resto das pautas.
    - Colocar Eventos de Click nos botões.
    - Mapear o o "submit" para mandar as informações para a API.

--- src/pages/Pautas/Step5/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Step5() {
-  return <h2>Step5</h2>;
-}
--- src/pages/Pautas/index.js
+++ src/pages/Pautas/index.js
@@ -1,51 +1,73 @@
 import React from "react"
-import { useForm, useStep } from "react-hooks-helper"
-
-import Step1 from './Step1'
-import Step2 from './Step2'
-import Step3 from './Step3'
-import Step4 from './Step4'
-import Step5 from './Step5'
-
 import "./styles.css"
+import {
+  Box,
+  Button,
+  Heading,
+  Flex,
+  Text
+} from '@chakra-ui/core'
 
-const steps = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 }
-]
+const CadastroPautas = () => (
+ 
+<Box>
+  <div id="TextoPautas">
+          <h2 id="text1">Pautas #MeRepresenta</h2>
+          <h2 id="text2">#LGBT+</h2>
+          <h2 id="text3">Pauta 01 de 22</h2>
+          <h2 id="text4">Discriminar LGBTs deve ser crime.</h2>
+          <h2 id="text5">O Brasil é um dos países que mais matam LGBTs no mundo, em especial travestis e <br></br> transexuais. Não há legislação que considere crime o preconceito contra lésbicas, transexuais <br></br> e travestis, bissexuais, gays. Propostas para tornar crime</h2>
+          <h2 id="text6">Qual o seu posicionamento?</h2>    
+    <div id="BotaoPautas">
+            <Button id="Button1">
+              Sou a<p id="FavorBold">FAVOR</p> da criminalização da LGBTfobia.
+            </Button>
+            <Button id="Button2">
+              Sou <p id="ContraBold">CONTRA</p> a criminalização da LGBTfobia.
+            </Button>
+    </div>
 
-const defaultData = {
-  firstName: "Jane",
-  lastName: "Doe",
-  nickName: "Jan",
-  address: "200 South Main St",
-  city: "Anytown",
-  state: "CA",
-  zip: "90505",
-  email: "email@domain.com",
-  phone: "+61 4252 454 332"
-}
+    <h2 id="text3">Pauta 02 de 22</h2>
+          <h2 id="text8">Trans e travestis poderem usar o banheiro que quiserem.</h2>
+          <h2 id="text7">A população transexual e travesti é frequentemente impedida de utilizar banheiros, <br></br> masculinos ou femininos em lugares públicos e comerciais. No Brasil, ainda não existe uma <br></br> legislação sobre esse assunto. Porém, o Supremo Tribunal Federal</h2>
+          <h2 id="text6">Qual o seu posicionamento?</h2>    
+    <div id="BotaoPautas2">
+            <Button id="Button3">
+              Sou a<p id="FavorBold">FAVOR</p> de pessoas trans e travestis poderem usar o banheiro que quiserem.
+            </Button>
+            <Button id="Button4">
+              Sou <p id="ContraBold">CONTRA</p> pessoas trans e travestis poderem usar o banheiro que quiserem.
+            </Button>
+    </div>
 
-const CadastroPautas = ({ images }) => {
-  const [formData, setForm] = useForm(defaultData);
-  const { step, navigation } = useStep({ initialStep: 0, steps })
-  const { id } = step
+    <h2 id="text10">Pauta 03 de 21</h2>
+          <h2 id="text9">Projeto “Escola sem partido”</h2>
+          <h2 id="text7">“Escola sem partido” é nome dado a uma série de projetos lei que têm sido apresentados <br></br> nos municípios, estados, no âmbito federal (PL 7180/2014), que querem retirar o ensino <br></br> sobre raça, gênero, identidade de gênero, orientação sexual e classe nas escolas. 73% dos <br></br> estudantes LGBTs já sofreu agressão verbal, 60% se sente inseguro e 36% já sofreu <br></br> violência física nas escolas. Um ministro do Supremo Tribunal Federal já suspendeu uma <br></br> dessas leis que tinha sido aprovada em Alagoas. Saiba mais a respeito com as campanhas
+          <a id="profLink" href="#">Professores contra o Escola Sem Partido</a> 
+          <a id="eLink">e</a> 
+          <a id="eduLink" href="#">Educação faz meu gênero.</a>
+          
+          <a id="SaberMais">Para saber ainda mais, assista esse</a> <a id="videoLink" href="#"> vídeo aqui.</a></h2>
 
-  const props = { formData, setForm, navigation };
+          <h2 id="text6">Qual o seu posicionamento?</h2>    
+    <div id="BotaoPautas2">
+            <Button id="Button3">
+              Sou a<p id="FavorBold">FAVOR</p> de pessoas trans e travestis poderem usar o banheiro que quiserem.
+            </Button>
+            <Button id="Button4">
+              Sou <p id="ContraBold">CONTRA</p> pessoas trans e travestis poderem usar o banheiro que quiserem.
+            </Button>
+    </div>
 
-  switch (id) {
-    case 1:
-      return <Step1 {...props} />
-    case 2:
-      return <Step2 {...props} />
-    case 3:
-      return <Step3 {...props} />
-    default:
-      return null
-  }
-};
+    <div id="divBetween">
+        <a id="voltarFormu" href="#">...Voltar para Formulário</a>
+        <Button id="botaoCont">Continuar</Button>
+    </div>
+
+</div>
+
+</Box>
+ 
+);
 
 export default CadastroPautas
